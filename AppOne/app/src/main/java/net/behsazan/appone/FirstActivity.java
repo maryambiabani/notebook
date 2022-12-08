a8626c124567cacf6bfd2bb031e1b973f89bb070
maryam biabani <maryambiabani@maryams-MacBook-Pro.local>
2022-12-08 11:13:50 -0800

    version4

--- AppOne/app/src/main/java/net/behsazan/appone/FirstActivity.java
+++ AppOne/app/src/main/java/net/behsazan/appone/FirstActivity.java
@@ -1,12 +1,19 @@
 package net.behsazan.appone;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 
+import com.google.android.material.navigation.NavigationView;
+
+import net.behsazan.ppone.R;
 import net.behsazan.ppone.databinding.ActivityFirstBinding;
 
 public class FirstActivity extends AppCompatActivity {
@@ -16,11 +23,46 @@ ActivityFirstBinding binding;
         super.onCreate(savedInstanceState);
         binding = ActivityFirstBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        binding.btnLogout.setOnClickListener(new View.OnClickListener() {
+        setSupportActionBar(binding.toolbarFirst);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(FirstActivity.this,binding.drawer
+                ,binding.toolbarFirst ,R.string.open , R.string.close );
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            toggle.getDrawerArrowDrawable().setColor(getColor(R.color.white));
+        } else {
+            toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.white));
+        }
+
+
+        toggle.syncState();
+
+       binding.navMenu.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+
+                    case R.id.item_add:
+
+                        Intent intent = new Intent(getApplicationContext() , HomeActivity.class);
+                        startActivity(intent);
+
+                        break;
+
+                    case R.id.item_logout:
+
+
+                        Intent intent1 = new Intent(getApplicationContext() , MainActivity.class);
+                        startActivity(intent1);
+
+
+                        break;
+
+
+                }
+
+                return false;
             }
         });
         binding.btnToDoList.setOnClickListener(new View.OnClickListener() {
